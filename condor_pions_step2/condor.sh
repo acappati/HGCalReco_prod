afd6e6e5d3cf3c5b2d324f100313eda5c92d024b
Alessandra Cappati <alessandra.cappati@cern.ch>
2024-05-08 19:40:08 +0200

    fix errors

--- condor_pions_step2/condor.sh
+++ condor_pions_step2/condor.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
 set -e
 
+export X509_USER_PROXY=/grid_mnt/vol_home/llr/cms/cappati/.t3/proxy.cert
+
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 cd /home/llr/cms/cappati/240329_TICLv5/CMSSW_14_0_0_pre0/
 cmsenv
 cd - 
 
-cat  <<EOF
-EOF >>CloseByParticle_Pion_GEN_SIM.py
+cat  <<EOF >>CloseByParticle_Pion_GEN_SIM.py
 from IOMC.RandomEngine.RandomServiceHelper import RandomNumberServiceHelper 
 randSvc = RandomNumberServiceHelper(process.RandomNumberGeneratorService) 
 randSvc.populate() 
 
-process.source.firstLuminosityBlock = cms.untracked.uint32($INDEX)
-
-
+process.source.firstLuminosityBlock = cms.untracked.uint32(${INDEX})
 EOF
 cmsRun CloseByParticle_Pion_GEN_SIM.py
 
-cat  <<EOF
-EOF >>step2_DIGI_L1TrackTrigger_L1_DIGI2RAW_HLT_PU.py
+cat  <<EOF >>step2_DIGI_L1TrackTrigger_L1_DIGI2RAW_HLT_PU.py
 from IOMC.RandomEngine.RandomServiceHelper import RandomNumberServiceHelper 
 randSvc = RandomNumberServiceHelper(process.RandomNumberGeneratorService) 
 randSvc.populate() 
@@ -27,6 +25,5 @@ EOF
 cmsRun step2_DIGI_L1TrackTrigger_L1_DIGI2RAW_HLT_PU.py
 
 mkdir -p /grid_mnt/data__data.polcms/cms/cappati/240507_pionsD98_step2/
-mv step2.root /grid_mnt/data__data.polcms/cms/cappati/240507_pionsD98_step2/step2_$INDEX.root
-EOF
-EOF
+mv step2.root /grid_mnt/data__data.polcms/cms/cappati/240507_pionsD98_step2/step2_${INDEX}.root
+
