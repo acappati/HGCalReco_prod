253159cbb9632aaa4b0672b4b7b39b2f8ce08e09
Alessandra Cappati <alessandra.cappati@cern.ch>
2024-05-16 19:05:07 +0200

    add dir for photon step2 prod

---
+++ condor_photons_step2/README.md
@@ -0,0 +1,16 @@
+# Condor setup
+
+Produces step1 files with the setup: CloseByParticle_Photon_GEN_SIM.py 
+
+and step2 files with the setup: step2_DIGI_L1TrackTrigger_L1_DIGI2RAW_HLT_PU.py
+
+1. Specify the particle quantities in the step1 configuration (PID, energy, ..)
+2. Specify the output directory for step2 files in condor.sh (step1 are not saved)
+3. in case, in condor.sh specify also the path where there is the environment used (L7)
+4. Specify the config files (CloseByParticle_Photon_GEN_SIM.py, step2_DIGI_L1TrackTrigger_L1_DIGI2RAW_HLT_PU.py) and the samples indexes in condor.sub
+5. Remember to create a directory called **logs** where the logs will be stored
+
+Run with: 
+```
+condor_submit condor.sub
+```
---
+++ condor_photons_step2/condor.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+export X509_USER_PROXY=/grid_mnt/vol_home/llr/cms/cappati/.t3/proxy.cert
+
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+cd /home/llr/cms/cappati/240516_ticlv5/CMSSW_14_1_X_2024-05-15-1100/
+cmsenv
+cd - 
+
+cat  <<EOF >>CloseByParticle_Photon_GEN_SIM.py
+from IOMC.RandomEngine.RandomServiceHelper import RandomNumberServiceHelper 
+randSvc = RandomNumberServiceHelper(process.RandomNumberGeneratorService) 
+randSvc.populate() 
+
+process.source.firstLuminosityBlock = cms.untracked.uint32(${INDEX})
+EOF
+cmsRun CloseByParticle_Photon_GEN_SIM.py
+
+cat  <<EOF >>step2_DIGI_L1TrackTrigger_L1_DIGI2RAW_HLT_PU.py
+from IOMC.RandomEngine.RandomServiceHelper import RandomNumberServiceHelper 
+randSvc = RandomNumberServiceHelper(process.RandomNumberGeneratorService) 
+randSvc.populate() 
+EOF
+cmsRun step2_DIGI_L1TrackTrigger_L1_DIGI2RAW_HLT_PU.py
+
+mkdir -p /grid_mnt/data__data.polcms/cms/cappati/240516_photonsD98_step2/
+mv step2.root /grid_mnt/data__data.polcms/cms/cappati/240516_photonsD98_step2/step2_${INDEX}.root
+
